47a0f88f3856b67734c278497c2cf368117fb52a
Sinclair Liu <nxp_nihao@163.com>
2021-10-16 22:42:20 +0800

    f

--- zq/zqkkz_speed1.js
+++ zq/zqkkz_speed1.js
@@ -106,13 +106,13 @@ Object.keys(zqlookStartbodys).forEach((item) => {
 //                 cookie_id = cookie.replace(/zqkey_id=/, "cookie_id=")
 //                 zq_cookie1= cookie_id  +'&device_brand=xfdg&device_id=cc7dgdsgfsz83e&device_model=1gx&device_platform=android&device_type=android&inner_version=202107261526&mi=0&openudid=cc7dgdsgfsz83e&os_api=27&os_version=bdftgsdfga&phone_network=WIFI&phone_sim=1'+'&request_time=' + time1 +'&time=' + time1 +'&'+ bodyVal
 //                 //console.log(`${zq_cookie1}`)
-            console.log(`--------看看赚上方宝箱奖励执行中--------\n`)
-            for(let k = 0; k < 3; k++){
-                id = k.toString()
-                await openbox(id,zq_cookie)
-                await $.wait(30000);
-            }
-            console.log("\n\n")
+            // console.log(`--------看看赚上方宝箱奖励执行中--------\n`)
+            // for(let k = 0; k < 3; k++){
+            //     id = k.toString()
+            //     await openbox(id,zq_cookie)
+            //     await $.wait(30000);
+            // }
+            // console.log("\n\n")
 
 //             }
 
